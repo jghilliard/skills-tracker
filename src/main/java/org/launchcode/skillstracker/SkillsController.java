de086bc3061459d997caef518534b9918640abd6
Josh Hilliard <125423113+jghilliard@users.noreply.github.com>
2023-04-20 19:50:28 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/skillstracker/SkillsController.java
@@ -0,0 +1,28 @@
+package org.launchcode.skillstracker;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class SkillsController {
+  @GetMapping()
+  @ResponseBody
+  public String helloForm() {
+    String html =
+            "<html>" +
+                    "<body>" +
+                    "<form method = 'get' action = '/hello'>" +
+                    "<input type = 'text' name = 'coder' />" +
+                    "<input type = 'submit' value = 'Greet Me!' />" +
+                    "</form>" +
+                    "</body>" +
+                    "</html>";
+    return html;
+  }
+
+  @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
+  @ResponseBody
+  public String hello(@RequestParam String coder) {
+    return "Hello, " + coder + "!";
+  }
+}
